4529ed2396831293b25598c858bd7fba4d8d942d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-07-07 13:23:23 -0400

    Added get for /firstname/lastname in server.js.

--- server.js
+++ server.js
@@ -8,5 +8,15 @@ app.get('/', function(request, response) {
   //send response back to page
   response.send('Hello World');
 });
+/*
+* Function that responds to /first/last request
+*/
+app.get('/:firstname/:lastname', function(request, response) {
+  //get firstname and last name from request
+  var first = request.params.firstname;
+  var last = request.params.lastname;
+  //send responce back
+  response.send(['Hello', first, last].join(' '));
+});
 //start the server with port and ip
 app.listen(process.env.PORT, process.env.IP);
